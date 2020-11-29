1b0f0e93b414e3afd288e738f86bbbf6b9718322
Serdar Balcı <drserdarbalci@gmail.com>
2020-11-29 20:55:29 +0300

    last 60 days

--- R/search.R
+++ R/search.R
@@ -1,7 +1,7 @@
 pancreas_articles <- RISmed::EUtilsSummary(query = searchFormula_pancreas,
                                      type = 'esearch',
                                      db = 'pubmed',
-                                     reldate = 10
+                                     reldate = 60
 )
 
 Sys.sleep(10)
@@ -9,7 +9,7 @@ Sys.sleep(10)
 gallbladder_articles <- RISmed::EUtilsSummary(query = searchFormula_gallbladder,
                                            type = 'esearch',
                                            db = 'pubmed',
-                                           reldate = 10
+                                           reldate = 60
 )
 
 Sys.sleep(10)
@@ -17,7 +17,7 @@ Sys.sleep(10)
 bileduct_articles <- RISmed::EUtilsSummary(query = searchFormula_bileduct,
                                            type = 'esearch',
                                            db = 'pubmed',
-                                           reldate = 10
+                                           reldate = 60
 )
 
 Sys.sleep(10)
@@ -25,7 +25,7 @@ Sys.sleep(10)
 ampulla_articles <- RISmed::EUtilsSummary(query = searchFormula_ampulla,
                                            type = 'esearch',
                                            db = 'pubmed',
-                                           reldate = 10
+                                           reldate = 60
 )
 
 Sys.sleep(10)
