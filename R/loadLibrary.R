887a4141002527a877a9d2b9f1847e2fc30db7c7
Serdar Balcı <drserdarbalci@gmail.com>
2020-11-29 08:18:37 +0300

    add libraries

--- R/loadLibrary.R
+++ R/loadLibrary.R
@@ -2,3 +2,4 @@
 suppressPackageStartupMessages(library("rmarkdown"))
 suppressPackageStartupMessages(library("readr"))
 suppressPackageStartupMessages(library("RefManageR"))
+suppressPackageStartupMessages(library("magrittr"))
