dcb6073b659c7d6dc09c321454ee711e9c06f77f
Serdar Balcı <drserdarbalci@gmail.com>
2020-12-01 14:27:02 +0300

    tic to mac

--- tic.R
+++ tic.R
@@ -1,17 +1,8 @@
 get_stage("install") %>%
   add_step(step_install_deps())
 
-print("stage install end")
-
-# source(file = "searchPubMed.R")
-
-# print("search pubmed end")
-
 get_stage("deploy") %>%
   add_code_step(rmarkdown::render_site())
-
-print("render site end")
-
 
 if (ci_on_ghactions()) {
   get_stage("before_deploy") %>%
@@ -20,5 +11,3 @@ if (ci_on_ghactions()) {
   get_stage("deploy") %>%
     add_step(step_push_deploy(path = "_site", branch = "gh-pages"))
 }
-
-print("push end")
